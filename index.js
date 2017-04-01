97d0c0efc852fb13f86036cb6d6c0e9cd538505d
DAVID <david.riudor@gmail.com>
2017-04-01 18:16:23 +0200

    restart

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const CONVERSATION_STATUS_OFERTAS = 2;
 const CONVERSATION_STATUS_DIRECCION = 3;
 const CONVERSATION_STATUS_PAGO = 4;
 const CONVERSATION_STATUS_FEEDBACK = 5;
-const CONVERSATION_STATUS_USUAL_USER = 6;
 let conversations = {};
 
 const ops = commandLineArgs([
@@ -63,60 +62,39 @@ controller.setupWebserver(process.env.port || 3000, (err,webserver) => {
     });
 });
 
-
-
-//controller.hears(['hola'], 'message_received', (bot, message) => {
-let flag= true ;
-controller.on('message_received', (bot, message) => {
-    if(flag){
-        flag = false ;
-        bot.startConversation(message, (err, convo) => {
-            convo.say('Hey! I am Deskie your cryptotrading assistant:)');
-            convo.say({
-                attachment: {
-                    'type':'template',
-                    'payload':{
-                        'template_type':'button',
-                        'text':'Do you know how can I help you?',
-                        'buttons':[
-                            {
-                                'type':'postback',
-                                'title':'Yes!',
-                                'payload':'Yes!'
-                            },
-                            {
-                                'type':'postback',
-                                'title':'No, I want to know more',
-                                'payload':'No:('
-                            }
-                        ]
-                    }
+controller.hears(['hola'], 'message_received', (bot, message) => {
+    bot.startConversation(message, (err, convo) => {
+        convo.say('Hola, somos instamaki.');
+        convo.say({
+            attachment: {
+                'type':'template',
+                'payload':{
+                    'template_type':'button',
+                    'text':'¿que podemos hacer por tí?',
+                    'buttons':[
+                        {
+                            'type':'postback',
+                            'title':'Ver ofertas',
+                            'payload':'ofertas'
+                        },
+                        {
+                            'type':'postback',
+                            'title':'Consultar locales',
+                            'payload':'locales'
+                        }
+                    ]
                 }
-            });
+            }
         });
-        conversations[message.channel] = {
-            status: CONVERSATION_STATUS_HELLO,
-            coordinates: undefined,
-            items: []
-        };
-    }
-}); 
-
-
-controller.hears(['yes!', 'si'], 'message_received', (bot, message) => {
-    // if(conversations[message.channel] && conversations[message.channel].status === CONVERSATION_STATUS_HELLO){
-        bot.startConversation(message, (err, convo) => {
-            
-
-            convo.say('Cool! Say me what you need');
-            conversations[message.channel].status = CONVERSATION_STATUS_USUAL_USER;
-            
-        });
-        flag= true;
-    // }
+    });
+    conversations[message.channel] = {
+        status: CONVERSATION_STATUS_HELLO,
+        coordinates: undefined,
+        items: []
+    };
 });
 
-controller.hears(['No:(', 'No'], 'message_received', (bot, message) => {
+controller.hears(['ofertas', 'pedido'], 'message_received', (bot, message) => {
     if(conversations[message.channel] && conversations[message.channel].status === CONVERSATION_STATUS_HELLO){
         bot.startConversation(message, (err, convo) => {
             convo.ask({
@@ -124,33 +102,83 @@ controller.hears(['No:(', 'No'], 'message_received', (bot, message) => {
                     'type':'template',
                     'payload':{
                         'template_type':'button',
-                        'text':'I can help you with any of this topics:',
+                        'text':'Actualmente tenemos disponible las siguientes ofertas:',
                         'buttons':[
                             {
                                 'type':'postback',
-                                'title':'How my day it is going',
-                                'payload':'my day'
+                                'title':'Oferta A: 12 Piezas por 8€',
+                                'payload':'A'
                             },
                             {
                                 'type':'postback',
-                                'title':'I want to buy/sell',
-                                'payload':'trade'
+                                'title':'Oferta B: 18 Piezas por 12€',
+                                'payload':'B'
                             },
                             {
                                 'type':'postback',
-                                'title':'See an specific crypto',
-                                'payload':'See'
+                                'title':'Oferta C: 24 Piezas por 18€',
+                                'payload':'C'
                             }
                         ]
                     }
                 }
             }, (response, convo2) => {
                 conversations[message.channel].items.push(response.text);
-                conversations[message.channel].status = CONVERSATION_STATUS_NO_USUAL_USER;
+                conversations[message.channel].status = CONVERSATION_STATUS_OFERTAS;
                 convo.next();
             });
-            
+            convo.say('Muy buena elección!');
+            convo.say('¿Donde quieres que te lo enviemos?');
+            convo.ask({
+                'text': 'Compartir ubicación',
+                'quick_replies': [
+                    {
+                        'content_type': 'location'
+                    }
+                ]
+            }, (response, convo2) => {
+            conversations[message.channel].status = CONVERSATION_STATUS_DIRECCION;
+                conversations[message.channel].coordinates = response.attachments[0].payload.coordinates;
+                convo.next();
+            });
+            convo.say({
+                "attachment":{
+                    "type":"template",
+                    "payload":{
+                        "template_type":"receipt",
+                        "recipient_name":"Gabriel Esteban",
+                        "order_number":"12345678902",
+                        "currency":"EUR",
+                        "payment_method":"Visa 2345",
+                        "timestamp":"1428444852",
+                        "elements":[
+                                {
+                                    "title":"Maki de 24 piezas",
+                                    "subtitle":"24 piezas surtidas de excelente calidad",
+                                    "quantity":1,
+                                    "price":18,
+                                    "currency":"EUR",
+                                    "image_url":"http://manekinekosushi.com/wp-content/uploads/2016/08/maki_29_-maki_mixto.jpg"
+                                }
+                        ],
+                        "address":{
+                            "street_1":"C/ Jordi Girona 1-3 Campus Diagonal Nord",
+                            "street_2":"Edific C6 Espai Empren",
+                            "city":"Barcelona",
+                            "postal_code":"08034",
+                            "state":"BCN",
+                            "country":"ES"
+                        },
+                        "summary":{
+                            "subtotal":21.6,
+                            "shipping_cost":2.95,
+                            "total_tax":2.4,
+                            "total_cost":26.95
+                        }
+                    }
+                }
+            });
+            conversations[message.channel].status = CONVERSATION_STATUS_PAGO;
         });
-        flag= true;
     }
 });

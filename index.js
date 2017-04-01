a7f8ff3ba0dc95c0e1135e7493150cdeba6aeb53
DAVID <david.riudor@gmail.com>
2017-04-01 17:18:44 +0200

    First flow trial

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const CONVERSATION_STATUS_OFERTAS = 2;
 const CONVERSATION_STATUS_DIRECCION = 3;
 const CONVERSATION_STATUS_PAGO = 4;
 const CONVERSATION_STATUS_FEEDBACK = 5;
+const CONVERSATION_STATUS_USUAL_USER = 6;
 let conversations = {};
 
 const ops = commandLineArgs([
@@ -68,23 +69,23 @@ controller.on('message_received', (bot, message) => {
     if(flag){
         flag = false ;
         bot.startConversation(message, (err, convo) => {
-            convo.say('Hola, somos instamaki.');
+            convo.say('Hey! I am Deskie your cryptotrading assistant:)');
             convo.say({
                 attachment: {
                     'type':'template',
                     'payload':{
                         'template_type':'button',
-                        'text':'¿que podemos hacer por tí?',
+                        'text':'Do you know how can I help you?',
                         'buttons':[
                             {
                                 'type':'postback',
-                                'title':'Ver ofertas',
-                                'payload':'ofertas'
+                                'title':'Yes!',
+                                'payload':'Yes!'
                             },
                             {
                                 'type':'postback',
-                                'title':'Consultar locales',
-                                'payload':'locales'
+                                'title':'No, I want to know more',
+                                'payload':'No:('
                             }
                         ]
                     }
@@ -98,6 +99,8 @@ controller.on('message_received', (bot, message) => {
         };
     }
 }); 
+
+
     
 
 
@@ -211,6 +214,58 @@ controller.hears(['ofertas', 'pedido'], 'message_received', (bot, message) => {
             flag = true ;
             counter= true ;
             conversations[message.channel].status = CONVERSATION_STATUS_PAGO;
+        });
+    }
+});
+
+
+
+controller.hears(['yes!', 'si'], 'message_received', (bot, message) => {
+    if(conversations[message.channel] && conversations[message.channel].status === CONVERSATION_STATUS_HELLO){
+        bot.startConversation(message, (err, convo) => {
+            
+
+            convo.say(''Cool! Say me what you need'');
+            conversations[message.channel].status = CONVERSATION_STATUS_USUAL_USER;
+            
+        });
+    }
+});
+
+controller.hears(['No:(', 'No'], 'message_received', (bot, message) => {
+    if(conversations[message.channel] && conversations[message.channel].status === CONVERSATION_STATUS_HELLO){
+        bot.startConversation(message, (err, convo) => {
+            convo.ask({
+                attachment: {
+                    'type':'template',
+                    'payload':{
+                        'template_type':'button',
+                        'text':'I can help you with any of this topics:',
+                        'buttons':[
+                            {
+                                'type':'postback',
+                                'title':'How my day it is going',
+                                'payload':'my day'
+                            },
+                            {
+                                'type':'postback',
+                                'title':'I want to buy/sell',
+                                'payload':'trade'
+                            },
+                            {
+                                'type':'postback',
+                                'title':'See an specific crypto',
+                                'payload':'See'
+                            }
+                        ]
+                    }
+                }
+            }, (response, convo2) => {
+                conversations[message.channel].items.push(response.text);
+                conversations[message.channel].status = CONVERSATION_STATUS_NO_USUAL_USER;
+                convo.next();
+            });
+            
         });
     }
 });
